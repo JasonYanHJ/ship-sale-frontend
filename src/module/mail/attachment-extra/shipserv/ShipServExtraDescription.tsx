e04de00a287a775f22a421c7f594aebd8f915f05
JasonYan <jasonyan1031@foxmail.com>
2025-07-28 15:47:32 +0800

    perf: 删去ShipServ询价附件的sections展示

--- src/module/mail/attachment-extra/shipserv/ShipServExtraDescription.tsx
+++ src/module/mail/attachment-extra/shipserv/ShipServExtraDescription.tsx
@@ -9,32 +9,9 @@ const ShipServExtraDescription = ({ extra }: { extra: ShipServExtra }) => {
     children: value,
   }));
 
-  const sectionItems: DescriptionsProps["items"][] = extra.section_data.map(
-    (section) =>
-      Object.entries(section).map(([key, value]) => ({
-        label: key,
-        children: value,
-      }))
-  );
-
   return (
     <div>
       <Descriptions colon={false} bordered size="small" items={metaItems} />
-      {sectionItems.length > 0 && (
-        <div style={{ marginTop: 8, marginLeft: 8 }}>
-          <b>sections:</b>
-        </div>
-      )}
-      {sectionItems.map((items, i) => (
-        <Descriptions
-          key={i}
-          style={{ marginTop: 8 }}
-          colon={false}
-          bordered
-          size="small"
-          items={items}
-        />
-      ))}
     </div>
   );
 };
